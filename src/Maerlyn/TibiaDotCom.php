a9b6e57762184c5df96ab5664bd4e5ac99adeda6
Maerlyn <m@maerlyn.eu>
2012-07-01 16:20:35 +0200

    add characterInfo function

--- src/Maerlyn/TibiaDotCom.php
+++ src/Maerlyn/TibiaDotCom.php
@@ -12,6 +12,72 @@ use Guzzle\Http\Client;
 class TibiaDotCom
 {
     /**
+     * Gets information about the given character
+     *
+     * @param string $name
+     */
+    public function characterInfo($name)
+    {
+        $html = $this->postUrl("http://www.tibia.com/community/?subtopic=characters", array("name" => $name));
+
+        if (false !== stripos($html, "<b>Could not find character</b>")) {
+            return false;
+        }
+
+        // this will be used later while we go through all the rows in the charinfo table
+        $map = array(
+            "Name:" => "name",
+            "Sex:" => "sex",
+            "Vocation:" => "vocation",
+            "Level:" => "level",
+            "World:" => "world",
+            "Residence:" => "residence",
+            "Achievement Points:" => "achievement_points",
+            "Last login:" => "last_login",
+            "Comment:" => "comment",
+            "Account Status:" => "account_status",
+            "Married to:" => "married_to",
+            "House:" => "house",
+            "Guild membership:" => "guild",
+        );
+
+        $domd = $this->getDOMDocument($html);
+        $domx = new \DOMXPath($domd);
+        $character = array();
+
+        $rows = $domx->query("//div[@class='BoxContent']/table[1]/tr[position() > 1]");
+        foreach ($rows as $row) {
+            $name  = trim($row->firstChild->nodeValue);
+            $value = trim($row->lastChild->nodeValue);
+
+            if (isset($map[$name])) {
+                $character[$map[$name]] = $value;
+            } else {
+                $character[$name] = $value;
+            }
+        }
+
+        // value cleanup
+
+        $character["last_login"] = \DateTime::createFromFormat("M d Y, H:i:s T", $character["last_login"]);
+
+        if (isset($character["guild"])) {
+            $values = explode(" of the ", $character["guild"]);
+            $character["guild"] = array(
+                "name"  =>  $values[1],
+                "rank"  =>  $values[0],
+            );
+        }
+
+        if (isset($character["house"])) {
+            $values = explode(" is paid until ", $character["house"]);
+            $character["house"] = $values[0];
+        }
+
+        return $character;
+    }
+
+    /**
      * Return the list of characters online at the given world
      *
      * @param type $world
